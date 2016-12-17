d61f0fc65347b48262c8a525349b7b6a65408891
BadKey <badkey.steam@gmail.com>
2016-12-17 17:03:43 +0100

    ItemChanges

--- Crime/src/me/seemslegit/crime/api/ItemAPI.java
+++ Crime/src/me/seemslegit/crime/api/ItemAPI.java
@@ -1,15 +1,22 @@
 package me.seemslegit.crime.api;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemAPI {
 	
-	public static ItemStack createStack(Material m, int Amount){
+	public static ItemStack createStack(Material m, int Amount, String displayname, String itemlorelore){
 		ItemStack s = new ItemStack(m, Amount);
 		ItemMeta meta = s.getItemMeta();
-		return null;
+		meta.setDisplayName(displayname);
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(itemlorelore);
+		meta.setLore(lore);
+		s.setItemMeta(meta);
+		return s;
 		
 	}
 
