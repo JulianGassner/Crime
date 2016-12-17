1a6bb67db66a6f1dbe6a744a0136359dc02caa60
Seems2Legit <drums22.mc@gmail.com>
2016-12-17 14:08:12 +0100

    Added "Random-Code" generator.

---
+++ Crime/src/me/seemslegit/crime/api/Code.java
@@ -0,0 +1,62 @@
+package me.seemslegit.crime.api;
+
+import java.util.Random;
+
+public class Code {
+
+	private String code = "";
+	private String raw = "";
+	private final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private Random rnd = new Random();
+
+	/**
+	 * 
+	 * @param length
+	 *            {@link Integer}
+	 */
+	public Code(int length) {
+		code = randomString(length);
+	}
+
+	/**
+	 * 
+	 * @return {@link String}
+	 */
+	public String getRaw() {
+		return raw;
+	}
+
+	/**
+	 * 
+	 * @param length
+	 *            {@link Integer}
+	 * @param prefix
+	 *            {@link String}
+	 */
+	public Code(int length, String prefix) {
+		raw = randomString(length);
+		code = prefix + raw;
+	}
+
+	/**
+	 * 
+	 * @param len
+	 *            {@link Integer}
+	 * @return {@link String}
+	 */
+	private String randomString(int len) {
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++)
+			sb.append(AB.charAt(rnd.nextInt(AB.length())));
+		return sb.toString();
+	}
+
+	/**
+	 * 
+	 * @return {@link String}
+	 */
+	public String getCode() {
+		return code;
+	}
+
+}
