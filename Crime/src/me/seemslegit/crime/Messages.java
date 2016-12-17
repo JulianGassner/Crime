d66d9446f417eb3e0ec42f4bd682ba4be5f11ad0
Seems2Legit <drums22.mc@gmail.com>
2016-12-17 14:25:44 +0100

    ....

--- Crime/src/me/seemslegit/crime/Messages.java
+++ Crime/src/me/seemslegit/crime/Messages.java
@@ -3,10 +3,10 @@ package me.seemslegit.crime;
 public final class Messages {
 
 	//General
-	public static String prefix = "Â§cCrime Â§8> ";
+	public static String prefix = "§cCrime §8> ";
 
 	//Player messages
-	public static String p_join  = "Â§8[Â§a+Â§8] %1% joined the game.";
-	public static String p_leave = "Â§8[Â§4-Â§8] %1% left the game.";
+	public static String p_join  = "§8[§a+§8] %1% joined the game.";
+	public static String p_leave = "§8[§4-§8] %1% left the game.";
 	
 }
