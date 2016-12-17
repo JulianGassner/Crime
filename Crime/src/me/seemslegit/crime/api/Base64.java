3cd2d2d150998b1168ff5697374499f5a1c4b937
Seems2Legit <drums22.mc@gmail.com>
2016-12-17 13:12:32 +0100

    .

--- Crime/src/me/seemslegit/crime/api/Base64.java
+++ Crime/src/me/seemslegit/crime/api/Base64.java
@@ -1,5 +1,103 @@
 package me.seemslegit.crime.api;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import me.seemslegit.crime.exceptions.DecodingException;
+import me.seemslegit.crime.plugin.Main;
+
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
 public class Base64 {
 
+	/**
+	 * 
+	 * @param o {@link Object}
+	 * @return {@link String}
+	 */
+	public static String toBase64(Object o) {
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(
+					outputStream);
+			dataOutput.writeObject(o);
+			dataOutput.close();
+			return Base64Coder.encodeLines(outputStream.toByteArray());
+		} catch (Exception e) {
+			Main.instance.getErrorManager().registerError(e);
+			throw new DecodingException();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param data {@link String}
+	 * @return {@link Object}
+	 */
+	public static Object FromBase64(String data) {
+		try{
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(
+					Base64Coder.decodeLines(data));
+			BukkitObjectInputStream dataInput = new BukkitObjectInputStream(
+					inputStream);
+			Object o = dataInput.readObject();
+			dataInput.close();
+			return o;
+		}catch(Exception e){
+			Main.instance.getErrorManager().registerError(e);
+			throw new DecodingException();
+		}
+	}
+
+	/**
+	 * 
+	 * @param data {@link String}
+	 * @return {@link Object}[]
+	 */
+	public static Object[] ArrayFromBase64(String data)
+			{
+		try {
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(
+					Base64Coder.decodeLines(data));
+			BukkitObjectInputStream dataInput = new BukkitObjectInputStream(
+					inputStream);
+			Object[] items = new Object[dataInput.readInt()];
+			for (int i = 0; i < items.length; i++) {
+				items[i] = dataInput.readObject();
+			}
+			dataInput.close();
+			return items;
+		} catch (Exception e) {
+			Main.instance.getErrorManager().registerError(e);
+			throw new DecodingException();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param items {@link Object}[]
+	 * @return {@link String}
+	 * @throws IllegalStateException
+	 */
+	public static String ArraytoBase64(Object[] items)
+			throws IllegalStateException {
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(
+					outputStream);
+
+			dataOutput.writeInt(items.length);
+			for (int i = 0; i < items.length; i++) {
+				dataOutput.writeObject(items[i]);
+			}
+			dataOutput.close();
+			return Base64Coder.encodeLines(outputStream.toByteArray());
+		} catch (Exception e) {
+			Main.instance.getErrorManager().registerError(e);
+			throw new DecodingException();
+		}
+	}
+	
 }
--- Crime/src/me/seemslegit/crime/plugin/Main.java
+++ Crime/src/me/seemslegit/crime/plugin/Main.java
@@ -1,9 +1,28 @@
 package me.seemslegit.crime.plugin;
 
+import me.seemslegit.crime.managment.ErrorManager;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 
+	public static Main instance;
 	
+	private ErrorManager mng_error;
+	
+	private void init() {
+		mng_error = new ErrorManager();
+		
+	}
+	
+	public void onEnable() {
+		instance = this;
+		
+		init();
+	}
+	
+	public ErrorManager getErrorManager() {
+		return mng_error;
+	}
 	
 }
