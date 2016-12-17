a69c4ac0da7b910131d7722fdb27b1504fdf2543
Seems2Legit <drums22.mc@gmail.com>
2016-12-17 17:52:31 +0100

    ItemAPI fix

--- Crime/src/me/seemslegit/crime/api/ItemAPI.java
+++ Crime/src/me/seemslegit/crime/api/ItemAPI.java
@@ -2,22 +2,90 @@ package me.seemslegit.crime.api;
 
 import java.util.ArrayList;
 
+import me.seemslegit.crime.decodings.Base64;
+
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
 
 public class ItemAPI {
 	
-	public static ItemStack createStack(Material m, int Amount, String displayname, String itemlorelore){
-		ItemStack s = new ItemStack(m, Amount);
-		ItemMeta meta = s.getItemMeta();
-		meta.setDisplayName(displayname);
-		ArrayList<String> lore = new ArrayList<String>();
-		lore.add(itemlorelore);
-		meta.setLore(lore);
-		s.setItemMeta(meta);
-		return s;
+	private Material mat = Material.AIR;
+	private short data = 0;
+	private int amout = 1;
+	private String display;
+	private String[] lore;
+	private String skullowner;
+	private Color leathercolor;
+	
+	public ItemAPI() {
 		
 	}
+	
+	public ItemAPI color(Color c) {
+		this.leathercolor = c;
+		return this;
+	}
+	
+	public ItemAPI lore(String... lore) {
+		this.lore = lore;
+		return this;
+	}
+	
+	public ItemAPI skullOwner(String owner) {
+		this.skullowner = owner;
+		return this;
+	}
+	
+	public ItemAPI displayName(String display) {
+		this.display = display;
+		return this;
+	}
+	
+	public ItemAPI data(short s) {
+		this.data = s;
+		return this;
+	}
+	
+	public ItemAPI material(Material m) {
+		this.mat = m;
+		return this;
+	}
+	
+	public ItemAPI amout(int a) {
+		this.amout = a;
+		return this;
+	}
+	
+	public ItemStack build() {
+		ItemStack i = new ItemStack(mat, amout, data);
+		ItemMeta im = i.getItemMeta();
+		if(display != null) im.setDisplayName(display);
+		if(lore != null) {
+			ArrayList<String> lore = new ArrayList<String>();
+			for(String s : this.lore) lore.add(s);
+			im.setLore(lore);
+		}
+		if(skullowner != null) {
+			SkullMeta sm = (SkullMeta) im;
+			sm.setOwner(skullowner);
+		}
+		if(leathercolor != null) {
+			LeatherArmorMeta lrm = (LeatherArmorMeta) im;
+			lrm.setColor(leathercolor);
+		}
+		
+		i.setItemMeta(im);
+		return i;
+	}
 
+	@Override
+	public String toString() {
+		return Base64.toBase64(build());
+	}
+	
 }
