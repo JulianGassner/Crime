f5fc12252f413a0c979b3857b016290c3fc6970a
Seems2Legit <drums22.mc@gmail.com>
2016-12-17 13:21:42 +0100

    .

--- Crime/src/me/seemslegit/crime/plugin/Main.java
+++ Crime/src/me/seemslegit/crime/plugin/Main.java
@@ -1,6 +1,7 @@
 package me.seemslegit.crime.plugin;
 
 import me.seemslegit.crime.managment.ErrorManager;
+import me.seemslegit.crime.managment.MoneyManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,9 +10,11 @@ public class Main extends JavaPlugin{
 	public static Main instance;
 	
 	private ErrorManager mng_error;
+	private MoneyManager mng_money;
 	
 	private void init() {
 		mng_error = new ErrorManager();
+		mng_money = new MoneyManager();
 		
 	}
 	
@@ -23,6 +26,10 @@ public class Main extends JavaPlugin{
 	
 	public ErrorManager getErrorManager() {
 		return mng_error;
+	}
+	
+	public MoneyManager getMoneyManager() {
+		return mng_money;
 	}
 	
 }
