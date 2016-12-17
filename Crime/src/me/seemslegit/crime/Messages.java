0a2107c7f3d730150beed7ca386f4b3e152b3f98
Seems2Legit <drums22.mc@gmail.com>
2016-12-17 14:15:01 +0100

    ..

---
+++ Crime/src/me/seemslegit/crime/Messages.java
@@ -0,0 +1,12 @@
+package me.seemslegit.crime;
+
+public final class Messages {
+
+	//General
+	public static String prefix = "";
+
+	//Player messages
+	public static String p_join  = "§8[§a+§8] %1% joined the game.";
+	public static String p_leave = "§8[§4-§8] %1% left the game.";
+	
+}
