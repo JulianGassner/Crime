0aaaf2fa010aeb2e16ffa1c45339b33b3f92c6a3
CodeBukkit <julian.gassner@student.htl-dornbirn.at>
2016-12-18 16:11:38 +0100

    Introducing drugs #959450534

--- Crime/src/me/seemslegit/crime/farming/FarmingManager.java
+++ Crime/src/me/seemslegit/crime/farming/FarmingManager.java
@@ -1,5 +1,19 @@
 package me.seemslegit.crime.farming;
 
+import org.bukkit.Bukkit;
+
+import me.seemslegit.crime.plugin.Main;
+
 public class FarmingManager {
 
+	
+	public FarmingManager() {
+		init();
+	}
+	
+	private void init() {
+		Bukkit.getPluginManager().registerEvents(new Drugs(), Main.instance);
+	}
+	
+	
 }
