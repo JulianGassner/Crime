6dedbc375e0db8e8b20f813947548e192a7eb61e
BadKey <badkey.steam@gmail.com>
2016-12-18 10:52:54 +0100

    Todo list + cop items extensions

--- Crime/src/me/seemslegit/crime/cop/CopItems.java
+++ Crime/src/me/seemslegit/crime/cop/CopItems.java
@@ -1,10 +1,23 @@
 package me.seemslegit.crime.cop;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import me.seemslegit.crime.api.ItemAPI;
 
 public class CopItems {
-	
+	/**
+	 * 
+	 * @param p {@link Player}
+	 */
 	public static void giveCopItems(Player p){
+		ItemStack copsword = new ItemAPI().material(Material.IRON_SWORD).displayName("§cSword of Law").build();
+		ItemStack handcuffs = new ItemAPI().material(Material.LEASH).displayName("§cHandcuffs").build();
+		ItemStack shears = new ItemAPI().material(Material.SHEARS).displayName("§aUncuff").build();
+		ItemStack tortch = new ItemAPI().material(Material.REDSTONE_TORCH_ON).displayName("§cClear illlegal items").build();
+		ItemStack 
+		
 		
 		
 	}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Crime
 Minecraft Crime
+
+Todo:
+- Cop API (handcuffs, send to jail with sword while handcuffed, itemclear, uncuff item)
+- If TnT Explodes - save blocks to rebuild
+- Declare jail location and send alarm to cops if a jail guy leaves jail cuffed or if TnT explodes in jail
+- Shops that can be robbed
+- Drugs
+- Plots
+- MiningAPI and mine function
+- Bread Farm
+- Disallow crafting (only for special custom recipes)
