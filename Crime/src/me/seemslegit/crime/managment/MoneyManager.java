b6d5cb45e4b901dea920378d13aa471dff91f4a0
Seems2Legit <drums22.mc@gmail.com>
2016-12-18 09:30:32 +0100

    Added JavaDoc

--- Crime/src/me/seemslegit/crime/managment/MoneyManager.java
+++ Crime/src/me/seemslegit/crime/managment/MoneyManager.java
@@ -8,10 +8,20 @@ public class MoneyManager {
 	
 	public static final float START_COINS = 100;
 	
+	/**
+	 * 
+	 * @param u {@link UserBase}
+	 * @return {@link Float}
+	 */
 	public float getCoins(UserBase u) {
 		return Float.parseFloat(u.getStats().getString("coins", START_COINS + ""));
 	}
 	
+	/**
+	 * 
+	 * @param u {@link UserBase}
+	 * @param coins {@link Float}
+	 */
 	public void setCoins(UserBase u, float coins) {
 		if(coins < 0) coins = 0;
 		if(coins > MAX_COINS) coins = MAX_COINS;
