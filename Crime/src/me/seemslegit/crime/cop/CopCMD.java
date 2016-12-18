6b0f8a13b50ead9b88b6101abedb28814b89d6cf
BadKey <badkey.steam@gmail.com>
2016-12-18 11:11:07 +0100

    Quick Fix

--- Crime/src/me/seemslegit/crime/cop/CopCMD.java
+++ Crime/src/me/seemslegit/crime/cop/CopCMD.java
@@ -3,6 +3,8 @@ package me.seemslegit.crime.cop;
 import me.seemslegit.crime.Messages;
 import me.seemslegit.crime.playerapi.User;
 
+import java.util.HashMap;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +12,9 @@ import org.bukkit.entity.Player;
 
 public class CopCMD implements CommandExecutor{
 
+	
+	private HashMap<String, Long> delay = new HashMap<String, Long>();
+	
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		if(!(cs instanceof Player)){
 			cs.sendMessage("You have to be a player !");
@@ -18,13 +23,36 @@ public class CopCMD implements CommandExecutor{
 		Player p = (Player) cs;
 		if(args[0].equalsIgnoreCase("switch")){
 			p.sendMessage(Messages.prefix+"§aYou switched successfully to §e"+CopManager.switchCop(new User(p)) + "§a!");
-			
+			CopItems.giveCopItems(p);
 		}if(args[0].equalsIgnoreCase("respawn")){
-			
+			if(hasCooldown(p.getUniqueId().toString())) {
+				
+			}else{
+				
+				
+				
+				delay.put(p.getUniqueId().toString(), System.currentTimeMillis() + 60*3 * 1000);
+			}
 		}
 		return true;
 	}
 	
+	
+	private boolean hasCooldown(String uuid) {
+		
+		if(delay.containsKey(uuid)) {
+			
+			long t = delay.get(uuid);
+			
+			if(System.currentTimeMillis() > t) {
+				delay.remove(uuid);
+				return false;
+			}
+			return true;
+		}
+		
+		return false;
+	}
 	
 
 }
--- Crime/src/me/seemslegit/crime/cop/CopItems.java
+++ Crime/src/me/seemslegit/crime/cop/CopItems.java
@@ -16,9 +16,15 @@ public class CopItems {
 		ItemStack handcuffs = new ItemAPI().material(Material.LEASH).displayName("§cHandcuffs").build();
 		ItemStack shears = new ItemAPI().material(Material.SHEARS).displayName("§aUncuff").build();
 		ItemStack tortch = new ItemAPI().material(Material.REDSTONE_TORCH_ON).displayName("§cClear illlegal items").build();
-		ItemStack 
+		ItemStack tnt = new ItemAPI().material(Material.TNT).displayName("§cC4").build();
+		//todo - Weapons
 		
-		
+		p.getInventory().clear();
+		p.getInventory().setItem(0, copsword);
+		p.getInventory().setItem(1, handcuffs);
+		p.getInventory().setItem(2, shears);
+		p.getInventory().setItem(3, tortch);
+		p.getInventory().setItem(4, tnt);
 		
 	}
 
